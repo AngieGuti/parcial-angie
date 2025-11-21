2a60cec12b0c41b4ad94b99b9ea15bb1f9c41bdf
Angie Gutierrez <ac.gutierrezt1@uniandes.edu.co>
2025-11-21 08:04:56 -0500

    ponente entidad

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PonenteModule } from './ponente/ponente.module';
 
 @Module({
-  imports: [],
+  imports: [PonenteModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/ponente/ponente.entity/ponente.entity.ts
@@ -0,0 +1,30 @@
+import {Entity, Column, PrimaryGeneratedColumn, Long } from 'typeorm';
+
+// Enum classs
+enum TipoPonente {
+    INTERNO = "Interno",
+    INVITADO = "Invitado"
+}
+
+@Entity()
+export class PonenteEntity {
+
+    @PrimaryGeneratedColumn()
+    id: Long;
+
+    @Column()
+    cedula: number;
+
+    @Column()
+    nombre: string;
+
+    @Column()
+    email: string;
+
+    @Column()
+    especialidad: string;
+
+    // Debe ser un string Interno o Invitado
+    @Column()
+    tipoPonente: TipoPonente;
+}
